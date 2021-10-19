97b4de0a481fb01cf1b0f9c0ea99bc0b930886e1
Azizul Hakim Shakil <azizulhakim.shakil18@gmail.com>
2021-10-19 02:17:15 +0200

    Final phase

--- Django-PostgreSQL-Project/asgi.py
+++ Django-PostgreSQL-Project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Django-Practice project.
+ASGI config for Django-PostgreSQL-Project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django-Practice.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django-PostgreSQL-Project.settings')
 
 application = get_asgi_application()
--- Django-PostgreSQL-Project/wsgi.py
+++ Django-PostgreSQL-Project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Django-Practice project.
+WSGI config for Django-PostgreSQL-Project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django-Practice.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django-PostgreSQL-Project.settings')
 
 application = get_wsgi_application()
